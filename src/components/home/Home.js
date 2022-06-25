35b2fc9dc13e0ba4c2fd4a7ed147bd88f947d8a9
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-06-25 22:33:44 +0600

    Complete Basic Structure

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Import Components
+
+// Import Styles
+import './home.css';
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    );
+}
+
+export default Home;
